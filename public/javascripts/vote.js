c412fd3e778736d1a7549f0f03be91dc417fb0a0
zhufengnodejs <zhufengnodejs@126.com>
2017-06-20 12:14:16 +0800

    上拉加载

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -42,7 +42,30 @@ $(function(){
         data:{offset,limit},
         dataType:'json',
         success(result){//是一个结果
+          offset += limit;//在加载一页成功之后改变offset值
           $('.coming').html(result.data.objects.map(user=>voteFn.formatUser(user)).join(''));
         }
     });
+    window.onscroll = function(){
+      //一屏的高度
+      let clientHeight = document.documentElement.clientHeight||document.body.clientHeight;
+      //当前内容向上卷去的高度
+      let scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+      //这是内容的高度
+      let contentHeight = document.documentElement.scrollheight || document.body.scrollHeight;
+      if(clientHeight+scrollTop>=contentHeight){
+          $.ajax({
+              url:'/vote/index/data',
+              type:'GET',
+              data:{offset,limit},
+              dataType:'json',
+              success(result){//是一个结果
+                  offset+=limit;
+                  $('.coming').append(result.data.objects.map(user=>voteFn.formatUser(user)).join(''));
+              }
+          });
+      }
+
+    }
+
 });
